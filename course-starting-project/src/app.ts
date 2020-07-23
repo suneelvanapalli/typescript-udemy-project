b0cc6308fff25a49701d789a6e91cb51a3277554
SuneelKumar Vanapalli <suneel.vanapalli@adudev.com>
2020-07-23 21:40:02 +1000

    tsc config

---
+++ course-starting-project/src/app.ts
@@ -0,0 +1,13 @@
+enum age {
+  new,
+  old,
+}
+
+const person: { name: string; age: number; hobbies: string[]; role: age } = {
+  name: 's',
+  age: 20,
+  hobbies: ['reading', 'cooking'],
+  role: age.new,
+};
+
+const nane: string = '';
---
+++ course-starting-project/src/log.ts
@@ -0,0 +1,3 @@
+let x = 'suneel';
+
+console.log(x);
